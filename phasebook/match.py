f996893ae876cef61e6965daa3262ee1b66ff139
DAndre06d <dumalayda06@gmail.com>
2024-07-14 16:42:02 +0800

    first commit

--- phasebook/match.py
+++ phasebook/match.py
@@ -3,25 +3,30 @@ from flask import Blueprint
 
 from .data.match_data import MATCHES
 
-
 bp = Blueprint("match", __name__, url_prefix="/match")
-
 
 @bp.route("<int:match_id>")
 def match(match_id):
+    # the data has length of 4
+    # this wil cehck if the user has entered a valid match id
     if match_id < 0 or match_id >= len(MATCHES):
         return "Invalid match id", 404
-
+    # this will be responsible for timestamp
     start = time.time()
     msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
     end = time.time()
 
     return {"message": msg, "elapsedTime": end - start}, 200
 
-
 def is_match(fave_numbers_1, fave_numbers_2):
+    """
+    this function is to cehck if there is a match. this will return a boolean.
+    used "set"
+    """
+    set_fave_numbers_1 = set(fave_numbers_1)
+    
     for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+        if number not in set_fave_numbers_1:
             return False
-
+    
     return True
--- phasebook/search.py
+++ phasebook/search.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+import json
 
 from .data.search_data import USERS
 
@@ -24,7 +25,22 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
-
     # Implement search here!
-
-    return USERS
+    filtereUsers = USERS
+    dataToSort = []
+    position = ("occupation", "age", "name", "id")
+    allKeys = set()
+    returned_value = []
+    # this will get all the keys in the data.
+    for user in USERS:
+        allKeys.update(user.keys())
+    for user in USERS:
+        for key, value in args.items():
+            if key in allKeys: 
+                if str(value).lower() in str(user.get(key)).lower():
+                    dataToSort.append(user)
+            else:
+                return {"message": "Invalid query parameters."}
+    # implemnt the sort here
+        
+    return  dataToSort
